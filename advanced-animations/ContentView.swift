6b09b0617802d78c7af8da08e088b142dd57e8c5
Laurent Brusa <wrmultitudes@gmail.com>
2023-07-24 17:50:37 +0200

    first commit
    created xcode project

---
+++ advanced-animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  advanced-animations
+//
+//  Created by Laurent B on 24/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ advanced-animations/advanced_animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  advanced_animationsApp.swift
+//  advanced-animations
+//
+//  Created by Laurent B on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct advanced_animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
